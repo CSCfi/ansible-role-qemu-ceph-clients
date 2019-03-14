2c326d458fb09cf848f1d7ee74e9bea438ef0210
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2019-03-14 13:55:59 +0200

    Inspect qemu monitor to verify rbd is used (#1)
    * Inspect qemu monitor to verify rbd is used
    
    When iterating qemu PIDs, verify from the qemu monitor 'info block'
    section that rbd driver is really being used for any of the VM disks.
    
    * Add commented debug
    
    Add commented out section that can be easily uncommented for
    getting information about configured RBDs.
    
    * Add comments
    
    Comment most things in the script. Also add more exhaustive
    command for retrieving yum history event IDs.
    
    * Use proper options and add some
    
    Move previous arguments to getopt for processing.
    
    Add a flag for excluding Qemus that don't have any RBDs. An example
    of this is would be e.g. an io.* VM which was booted in Jewel era
    and does not have any volumes attached.
    
    Add a debug flag which allows outputting individual RBD counts (since
    that seems to correlate nicely to the number that `ceph --features`
    outputs.
    
    * Fix getopt params
    
    Also make debug configurable from role variable
    
    * Fix logic
    
    Things make more sense if we by default include rbd-less qemus
    and change the optional parameter to exclude.
    
    * Add debug and exclude as proper ansible variables
    
    * Fix variable naming
    
    * Change include into exclude
    
    Using an exclude argument to control an include variable was a
    bit convoluted, so sort that out. Also add a "Managed by Ansible"
    header.
    
    * Move variables from vars to default
    
    Also rename them to something hopefully better. Also fix an
    error confusing a literal 'true' to boolean true.
    
    * Fix a quoting issue
    
    Instead of quoting individual "{{statements}}" quote the whole
    command.

--- files/ceph-version-qemu.sh
+++ files/ceph-version-qemu.sh
@@ -1,40 +1,95 @@
 #!/bin/bash
+# Managed with Ansible
 
-# By default, search for upgrades from Jewel to Luminous.
+# DEFAULTS
+# Search for upgrades from Jewel to Luminous.
 CEPH_MAJORVER_EXPECTED_OLD=10
 CEPH_MAJORVER_EXPECTED_NEW=12
+# QEMUs with no Rados Block Devices (RBD) are included in the results.
+EXCLUDE_QEMU_WITHOUT_RBD=1
+# No debug logging.
+DEBUG=1
 
-# Expect that Ceph was upgraded from major version $1
-if [ -z ${CEPH_MAJORVER_EXPECTED_OLD+x} ]; then
-  CEPH_MAJORVER_EXPECTED_OLD=$1
-fi
-# Expect that Ceph was upgraded from major version $2
-if [ -z ${CEPH_MAJORVER_EXPECTED_NEW+x} ]; then
-  CEPH_MAJORVER_EXPECTED_NEW=$2
-fi
+usage() {
+  echo "Usage: $0 [-o 10] [-n 12] [-e] [-d]" 1>&2;
+  echo "" 1>&2;
+  echo "  -o, expected old ceph version" 1>&2;
+  echo "  -n, expected new ceph version" 1>&2;
+  echo "  -e, exclude QEMUs with no RBDs" 1>&2;
+  echo "  -d, debug" 1>&2;
+  echo "" 1>&2;
+  exit 1
+}
 
-YUM_HISTORY_EVENTS=$(yum history|grep -E '^  '|awk '{print $1}')
+while getopts ":o:n:edh" arg; do
+  case $arg in
+    o)
+       CEPH_MAJORVER_EXPECTED_OLD=$OPTARG
+       ;;
+    n)
+       CEPH_MAJORVER_EXPECTED_NEW=$OPTARG
+       ;;
+    e)
+       EXCLUDE_QEMU_WITHOUT_RBD=0
+       ;;
+    d)
+       DEBUG=0
+       ;;
+    h|*)
+       usage
+       exit 0
+       ;;
+  esac
+done
+
+# Retrieve a list of all yum history event IDs
+YUM_HISTORY_EVENTS=$(yum history list all|grep -Po "^\s+\d+")
 
 for YUM_HISTORY_EVENT in $YUM_HISTORY_EVENTS; do
   CEPH_MAJORVER_OLD=$(yum history info $YUM_HISTORY_EVENT|grep -A1 ceph-common|head -1|awk -F':' '{print $2}'|awk -F'.' '{print $1}')
   CEPH_MAJORVER_NEW=$(yum history info $YUM_HISTORY_EVENT|grep -A1 ceph-common|tail -1|awk -F':' '{print $2}'|awk -F'.' '{print $1}')
+  # See if this event matches the upgrade path we're looking for
   if [[ "$CEPH_MAJORVER_OLD" == "$CEPH_MAJORVER_EXPECTED_OLD" ]] && [[ "$CEPH_MAJORVER_NEW" == "$CEPH_MAJORVER_EXPECTED_NEW" ]]
   then
+    # It did, so mark down event time
     CEPH_UPGRADE_DATE=$(yum history info $YUM_HISTORY_EVENT|grep '^Begin time'|awk -F' : ' '{print $2}')
     CEPH_UPGRADE_TIMESTAMP=$(date -d "$CEPH_UPGRADE_DATE" +%s)
     break
   fi
 done
 
+# Retrieve a list of all QEMU process IDs
 PIDS=$(pidof /usr/libexec/qemu-kvm)
 
 for PID in $PIDS; do
+  # Get QEMU process start time
   QEMU_TIMESTAMP=$(date -d "$(stat -c %x /proc/$PID/stat)" +%s)
+  # Get virsh instance name
+  INSTANCE_NAME=$(ps -ef -q $PID|grep -Po "(instance-\w+)"|uniq)
+  # Look for presence of block devices with "rbd" driver (as opposed to "raw" for local)
+  INSTANCE_QEMU_MON_RBD=$(virsh qemu-monitor-command --hmp "$INSTANCE_NAME" 'info block'|grep rbd)
+  # Save return code for determining if rbd's were present
+  INSTANCE_HAS_RBD=$?
+  # Check if instance did NOT have RBDs
+  if [[ "$INSTANCE_HAS_RBD" -ne "0" ]]
+  then
+    # Check if these type of instances should be excluded from search results
+    if [[ "$EXCLUDE_QEMU_WITHOUT_RBD" -eq "0" ]]
+    then
+      break
+    fi
+  fi
+
+  # Check if QEMU process has been running before the Ceph upgrade
   if [[ "$QEMU_TIMESTAMP" -lt "$CEPH_UPGRADE_TIMESTAMP" ]]
   then
-    INSTANCE_NAME=$(ps -ef -q $PID|grep -Po "(instance-\w+)"|uniq)
+    # If it did, print out the instance UUID.
     INSTANCE_UUID=$(virsh dumpxml $INSTANCE_NAME|grep \/uuid|awk -F'>' "{print \$2}"|awk -F'<' "{print \$1}")
     echo $INSTANCE_UUID
+    if [[ "$DEBUG" -eq "0" ]]
+    then
+      INSTANCE_QEMU_MON_RBD_COUNT=$(virsh qemu-monitor-command --hmp "$INSTANCE_NAME" 'info block'|grep -c rbd)
+      echo "DEBUG: $INSTANCE_UUID has this many RBD devices configured: $INSTANCE_QEMU_MON_RBD_COUNT"
+    fi
   fi
 done
-
